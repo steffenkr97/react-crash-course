d823a45fc2c78a9c578497a5d12381e511131d21
Steffen Kroll <kroll.steffen@icloud.com>
2023-05-15 21:17:52 +0200

    sec-18

--- frontend-react/src/components/Controller.tsx
+++ frontend-react/src/components/Controller.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function Controller() {
   const myFunc = () => {};
 
-  return <div>Controller</div>;
+  return (
+    <div className=" container bg-green-300 mx-auto">
+      <button className="transition-all duration-300 border border-black w-full mt-4 px-4 py-4 rounded-sm hover:bg-indigo-600 bg-indigo-400 text-white">
+        Place Bet
+      </button>
+    </div>
+  );
 }
 
 export default Controller;
