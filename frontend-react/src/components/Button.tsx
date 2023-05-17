2ae0571aa67684758eb52d8fc28acc56eec339dc
Steffen Kroll <kroll.steffen@icloud.com>
2023-05-17 16:08:19 +0200

    sec-25

--- frontend-react/src/components/Button.tsx
+++ frontend-react/src/components/Button.tsx
@@ -6,7 +6,7 @@ function Button( {runFunction}: Props ) {
   return (
     <button
       onClick={runFunction}
-      className="transition-all duration-300 border border-black w-full px-4 py-12 rounded-sm hover:bg-indigo-600 bg-indigo-400 text-white"
+      className="transition-all duration-300 border border-black w-full px-4 py-12 rounded-lg hover:bg-indigo-600 bg-indigo-400 text-white"
     >
       Place Bet
     </button>
