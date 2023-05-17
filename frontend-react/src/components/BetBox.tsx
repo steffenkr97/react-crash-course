70feee1c4aafde45b198c54152d267aa5cf03a9b
Steffen Kroll <kroll.steffen@icloud.com>
2023-05-17 10:15:38 +0200

    sec-22

--- frontend-react/src/components/BetBox.tsx
+++ frontend-react/src/components/BetBox.tsx
@@ -1,14 +1,29 @@
-function BetBox() {
+import BetButton from "./BetButton";
+
+type Props = {
+  betDirection: string;
+  setBetDirection: any;
+};
+
+function BetBox({ betDirection, setBetDirection }: Props) {
   return (
     <div className="mt-5">
-        <div className="py-2 border bg-red-100">LAYER 1</div>
-        <div className="flex flex-row justify-between py-2 border bg-blue-100">
-            <div className="bg-yellow-200 w-full py-12 text-center">BET BUTTON 1</div>
-            <div className="bg-green-200 w-full py-12 text-center">BET BUTTON 2</div>
-        </div>
-        <div className="py-2 border bg-fuchsia-100">LAYER 3</div>
+      <div className="py-2 border bg-red-100">LAYER 1</div>
+      <div className="flex flex-row justify-between py-2 border">
+        <BetButton
+          direction="down"
+          betDirection={betDirection}
+          setBetDirection={setBetDirection}
+        />
+        <BetButton
+          direction="up"
+          betDirection={betDirection}
+          setBetDirection={setBetDirection}
+        />
+      </div>
+      <div className="py-2 border bg-fuchsia-100">LAYER 3</div>
     </div>
-  )
+  );
 }
 
-export default BetBox
+export default BetBox;
--- frontend-react/src/components/Controller.tsx
+++ frontend-react/src/components/Controller.tsx
@@ -4,19 +4,20 @@ import BetBox from "./BetBox";
 
 function Controller() {
   const [hasWon, setHasWon] = useState(false);
+  const [betDirection, setBetDirection] = useState("up");
 
   const handlePlaceBet = () => {
     setHasWon(!hasWon);
   };
   
   useEffect(() => {
-    console.log(hasWon);
-  }, [hasWon]);
+    console.log(betDirection);
+  }, [betDirection]);
 
   return (
-    <div className="w-full md:w-[850] lg:w-[1200px] py-12 bg-green-300 mx-auto px-5">
+    <div className="w-full md:w-[850] lg:w-[1200px] py-12 mx-auto px-5">
       <Button runFunction={handlePlaceBet} />
-      <BetBox />
+      <BetBox betDirection={betDirection} setBetDirection={setBetDirection} />
     </div>
   );
 }
